66223be0c49e40ad770caa28a249d9e5a3c1d403
Steven <stevenvo777@gmail.com>
2025-01-07 20:43:29 -0600

    Added basic controller methods

--- server/Controllers/Request_Solver_Controller.cs
+++ server/Controllers/Request_Solver_Controller.cs
@@ -20,12 +20,40 @@ namespace RequestSolver.Controllers
             _context = context;
         }
 
-        [HttpGet("Users")]
-        public async Task<ActionResult<List<User>>> GetUsers()
+        [HttpGet("users")]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
         {
             var users = await _context.Users.ToListAsync();
             return Ok(users);
         }
 
+        [HttpGet("users/{user_id}")]
+        public async Task<ActionResult<User>> GetUser(int user_id)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(user => user.user_id == user_id);
+            if (user == null) {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpGet("requests")]
+        public async Task<ActionResult<List<Request>>> GetRequests()
+        {
+            var requests = await _context.Requests.ToListAsync();
+            return Ok(requests);
+        }
+
+        [HttpGet("requests/assigned_requests/{user_id}")]
+        public async Task<ActionResult<List<Request>>> GetAllRequestForUser(int user_id)
+        {
+            var userExists = await _context.Users.SingleOrDefaultAsync(user => user.user_id == user_id);
+            if (userExists == null)
+            {
+                return NotFound();
+            }
+            var requestsForUser = await _context.Requests.Where(rq => rq.assigned_to_user_id == user_id).ToListAsync();
+            return Ok(requestsForUser);
+        }
     }
 }
